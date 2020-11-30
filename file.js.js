0bb2cfda370f37639d76e858dd9e44611336de77
vantablanta <njerimwanjiku@gmail.com>
2020-11-30 19:37:19 +0300

    mmm

--- file.js.js
+++ file.js.js
@@ -291,4 +291,38 @@ let todoList = ["cook", "clean", "shower"];
  };   
  /*console.log(remember());
  console.log(getTask());*/
+<<<<<<< HEAD
  console.log(rememberUrgently());
+=======
+ console.log(rememberUrgently());
+
+ let array =[0, 1, 2, 3, 4,5]
+ console.log(array.slice(0,4));
+
+
+ function range(start, end, step = start < end ? 1 : -1) {
+  let array = [];
+
+  if (step > 0) {
+    for (let i = start; i <= end; i += step) array.push(i);
+  } else {
+    for (let i = start; i >= end; i += step) array.push(i);
+  }
+  return array;
+}
+
+function sum(array) {
+  let total = 0;
+  for (let value of array) {
+    total += value;
+  }
+  return total;
+}
+
+console.log(range(1, 10))
+// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(range(5, 2, -1));
+// → [5, 4, 3, 2]
+console.log(sum(range(1, 10)));
+// → 55
+>>>>>>> 339722a (js)
